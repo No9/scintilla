f2f8553d6fa4a5ab03597941f7ed6720429cf23f
No9 <mrcowsy@gmail.com>
2013-08-11 15:32:40 +0100

    prevent no tests dir but

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var db = level('tests/.db')
 
 setInterval(function(){
 
-db.put('name', 'level', function(err) {
+db.put('name', { "level" : "FTW" }, function(err) {
   if(err) return console.log(err)
    console.log("put") 
   })
